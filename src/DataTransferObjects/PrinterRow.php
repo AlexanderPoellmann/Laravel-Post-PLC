4f126c31403b1c80ba4d095d37ad909b1db7fab8
Alexander Manfred Pöllmann <AlexanderPoellmann@users.noreply.github.com>
2023-09-20 16:52:52 +0200

    Define printer row fields

--- src/DataTransferObjects/PrinterRow.php
+++ src/DataTransferObjects/PrinterRow.php
@@ -6,4 +6,11 @@ use Spatie\LaravelData\Data;
 
 class PrinterRow extends Data
 {
+    public function __construct(
+        public readonly ?string $LanguageID,
+        public readonly ?string $LabelFormatID,
+        public readonly ?string $PaperLayoutID,
+        public readonly ?string $Encoding,
+    ) {
+    }
 }
