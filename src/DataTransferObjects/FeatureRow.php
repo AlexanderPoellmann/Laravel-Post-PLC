f1a3f70395147476254ff18204ec9e77f845e7dc
Alexander Manfred Pöllmann <AlexanderPoellmann@users.noreply.github.com>
2023-09-20 19:29:40 +0200

    Add features

--- src/DataTransferObjects/FeatureRow.php
+++ src/DataTransferObjects/FeatureRow.php
@@ -2,8 +2,17 @@
 
 namespace AlexanderPoellmann\LaravelPostPlc\DataTransferObjects;
 
+use AlexanderPoellmann\LaravelPostPlc\Enums\Features;
 use Spatie\LaravelData\Data;
 
 class FeatureRow extends Data
 {
+    public function __construct(
+        public readonly Features $ThirdPartyID,
+        public readonly ?string $Value1,
+        public readonly ?string $Value2,
+        public readonly ?string $Value3,
+        public readonly ?string $Value4,
+    ) {
+    }
 }
